68e723aeaebf039f6e48528d461933b220818240
amrelskhawy <amrelskhawy@gmail.com>
2023-06-02 19:26:56 +0300

    customize primeng components

--- src/app/components/exchanger/exchanger.component.ts
+++ src/app/components/exchanger/exchanger.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiCallingService } from "../../services/api-calling.service"
-import Currency from "../../../interfaces/currencyApi"
+import Currency from "../../interfaces/currencyApi"
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-exchanger',
   templateUrl: './exchanger.component.html',
 })
 export class ExchangerComponent implements OnInit {
-  fromVal: string = 'EUR'
+  fromVal: string = '14'
   selectedFromCurrency: string = ''
-  toVal: string= 'USD'
+  toVal: string= '15'
   dataApi?: Currency;
   rates!: Array<string>;
+  currentCurrecyPath!: String
 
   countries?: any;
   selectedCity?: any;
 
-  onSwap(toVal: string, fromVal: string) {
+  onSwap( fromVal: string, toVal: string) {
     this.fromVal = toVal;
-    // this.toVal = fromVal;
+    this.toVal = fromVal;
+
+    console.log(`From ${this.fromVal} and To ${this.toVal}`);
+
   }
-  constructor(private apis: ApiCallingService) { }
+
+  constructor(private apis: ApiCallingService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
     let dataA =  this.apis.getData().subscribe(data => {
@@ -30,81 +37,11 @@ export class ExchangerComponent implements OnInit {
 
     })
 
-    this.countries = [
-      {
-        name: 'Australia',
-        code: 'AU',
-        states: [
-          {
-            name: 'New South Wales',
-            cities: [
-              { cname: 'Sydney', code: 'A-SY' },
-              { cname: 'Newcastle', code: 'A-NE' },
-              { cname: 'Wollongong', code: 'A-WO' }
-            ]
-          },
-          {
-            name: 'Queensland',
-            cities: [
-              { cname: 'Brisbane', code: 'A-BR' },
-              { cname: 'Townsville', code: 'A-TO' }
-            ]
-          }
-        ]
-      },
-      {
-        name: 'Canada',
-        code: 'CA',
-        states: [
-          {
-            name: 'Quebec',
-            cities: [
-              { cname: 'Montreal', code: 'C-MO' },
-              { cname: 'Quebec City', code: 'C-QU' }
-            ]
-          },
-          {
-            name: 'Ontario',
-            cities: [
-              { cname: 'Ottawa', code: 'C-OT' },
-              { cname: 'Toronto', code: 'C-TO' }
-            ]
-          }
-        ]
-      },
-      {
-        name: 'United States',
-        code: 'US',
-        states: [
-          {
-            name: 'California',
-            cities: [
-              { cname: 'Los Angeles', code: 'US-LA' },
-              { cname: 'San Diego', code: 'US-SD' },
-              { cname: 'San Francisco', code: 'US-SF' }
-            ]
-          },
-          {
-            name: 'Florida',
-            cities: [
-              { cname: 'Jacksonville', code: 'US-JA' },
-              { cname: 'Miami', code: 'US-MI' },
-              { cname: 'Tampa', code: 'US-TA' },
-              { cname: 'Orlando', code: 'US-OR' }
-            ]
-          },
-          {
-            name: 'Texas',
-            cities: [
-              { cname: 'Austin', code: 'US-AU' },
-              { cname: 'Dallas', code: 'US-DA' },
-              { cname: 'Houston', code: 'US-HO' }
-            ]
-          }
-        ]
-      }
-    ];
 
+    this.route.url.subscribe(params => {
+      this.currentCurrecyPath = params[1]['path']
+
+    })
   }
 
 
