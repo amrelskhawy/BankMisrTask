fc920312d5bac8e99526b03a55495aa5a6bf2db7
amrelskhawy <amrelskhawy@gmail.com>
2023-06-02 19:30:00 +0300

    Adds title inputs that recived page title

--- src/app/components/currency-convertor/currency-convertor.component.ts
+++ src/app/components/currency-convertor/currency-convertor.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-currency-convertor',
   templateUrl: './currency-convertor.component.html',
-  styleUrls: ['./currency-convertor.style.css']
 })
 export class CurrencyConvertorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
+  @Input() title: any = ''
+  currentCurrecyPath!: string
 
   amount: number = 1;
 
   ngOnInit() {
+    this.route.url.subscribe(params =>
 
+      this.currentCurrecyPath = params[1]['path']
+    )
   }
+
 
   onChange() {
     console.log(this.amount);
