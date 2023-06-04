4daa6bb3cc3ba541adf60bb50db811808339443a
amrelskhawy <amrelskhawy@gmail.com>
2023-06-04 20:46:17 +0300

    change interface properties to the new Apis

--- src/app/interfaces/currencyApi.ts
+++ src/app/interfaces/currencyApi.ts
@@ -1,7 +1,24 @@
 export default interface Currency {
-  success:   boolean;
-  timestamp: number;
-  base:      string;
-  date:      Date | string;
-  rates:     { [key: string]: number };
+  motd:       MOTD;
+  success:    boolean;
+  query:      Query;
+  info:       Info;
+  historical: boolean;
+  date:       Date;
+  result:     number;
+}
+
+export interface Info {
+  rate: number;
+}
+
+export interface MOTD {
+  msg: string;
+  url: string;
+}
+
+export interface Query {
+  from:   string;
+  to:     string;
+  amount: number;
 }
