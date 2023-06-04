378e7f7a525cc54e51cd07ba3d3286044a2b7e57
amrelskhawy <amrelskhawy@gmail.com>
2023-06-04 22:42:36 +0300

    Adds Dynamics in title depends on params of url

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ApiCallingService} from "../../services/api-calling.service";
+import SymbolInterface from "../../interfaces/Symbol.interface";
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  currency?: string
+  currencySymbol?: string
+  symbols?: SymbolInterface
+
+  currencyTitle?: string
+
+  constructor(private route: ActivatedRoute, private Api: ApiCallingService) {
+  }
+
+  ngOnInit() {
+
+    this.route.url.subscribe(data => this.currency = data[1].path )
+
+    this.Api.getSymbols().subscribe(data => {
+        this.symbols = data as SymbolInterface
+        this.currencyTitle = `${this.symbols.symbols[this.currency as string]['code']}  ${this.symbols.symbols[this.currency as string]['description']}`
+      }
+
+    )
+
+  }
+
 
 }
