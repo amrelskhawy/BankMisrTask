f02d353faa34c9ec48123d73a1d6d74b00df8dae
amrelskhawy <amrelskhawy@gmail.com>
2023-06-05 14:48:12 +0300

    Adds Components Module to take all components

--- src/app/components/my-components.module.ts
+++ src/app/components/my-components.module.ts
@@ -1,10 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NavbarComponent} from "./navbar/navbar.component";
+import {HomeComponent} from "../pages/home/home.component";
+import {DetailsComponent} from "../pages/details/details.component";
+import {CurrencyConvertorComponent} from "./currency-convertor/currency-convertor.component";
+import {ExchangerComponent} from "./exchanger/exchanger.component";
+import { FormsModule } from '@angular/forms';
+import { NgOptimizedImage } from "@angular/common";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    NgOptimizedImage,
+    MatSelectModule,
+    MatButtonModule,
+    RouterLink,
+    MatProgressSpinnerModule
   ],
-  declarations: []
+  declarations: [
+    NavbarComponent,
+    HomeComponent,
+    DetailsComponent,
+    CurrencyConvertorComponent,
+    ExchangerComponent
+  ],
+  exports: [
+    NavbarComponent,
+    HomeComponent,
+    DetailsComponent,
+    CurrencyConvertorComponent,
+    ExchangerComponent,
+    FormsModule,
+    NgOptimizedImage
+  ]
 })
 export class MyComponentsModule { }
