61048174157437bde83e0377c1e4bec1e8e4f152
amrelskhawy <amrelskhawy@gmail.com>
2023-06-05 14:21:27 +0300

    Add Material Components at Angular material module

--- src/app/modules/angular-material.module.ts
+++ src/app/modules/angular-material.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule
 
   ],
   exports: [
     MatSelectModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDividerModule
   ]
 })
 export class AngualrMaterialModule {  }
